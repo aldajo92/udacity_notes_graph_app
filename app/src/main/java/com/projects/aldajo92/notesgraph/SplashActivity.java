b695b87ceef33ed1d4e05d59d87828c0b072cf3f
Alejandro Gomez <alejodiscovery20@gmail.com>
2019-09-30 01:42:16 -0500

    updated firebase configuration

--- app/src/main/java/com/projects/aldajo92/notesgraph/SplashActivity.java
+++ app/src/main/java/com/projects/aldajo92/notesgraph/SplashActivity.java
@@ -3,28 +3,114 @@ package com.projects.aldajo92.notesgraph;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
-import com.projects.aldajo92.notesgraph.login.LoginActivity;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.projects.aldajo92.notesgraph.main.HomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     public static int SPLASH_DURATION = 1000;
 
+    private static final int RC_SIGN_IN = 0X17;
+
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private SignInButton button;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(this::launchMainActivity, SPLASH_DURATION);
-//        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.button_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        button.setOnClickListener(v -> signIn());
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        validateLogin(currentUser);
+        updateUI(currentUser);
     }
 
     private void launchMainActivity(){
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Log.w("ADJGF", "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount signInAccount) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                        validateLogin(currentUser);
+                        updateUI(currentUser);
+                    } else {
+                        Snackbar.make(findViewById(R.id.layout_splash), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void validateLogin(@Nullable FirebaseUser account){
+        if (account != null) {
+            new Handler().postDelayed(() -> launchMainActivity(), SPLASH_DURATION);
+        }
+    }
+
+    private void updateUI(@Nullable FirebaseUser account) {
+        if (account != null) {
+            button.setVisibility(View.GONE);
+        } else {
+            button.setVisibility(View.VISIBLE);
+        }
     }
 }
